86c4d7c3a24990cc7485a3039b55d510a4368d74
AR <arseneyr@users.noreply.github.com>
2020-05-13 20:51:04 -0700

    adding example

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
+const largeData = createSlice({
+  name: "largeData",
+  initialState: null,
+  reducers: {
+    setLargeData: (state, { payload }) => payload,
   },
 });
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    largeData: largeData.reducer,
+  },
+  devTools: {
+    stateSanitizer: (state) => ({ ...state, largeData: "Replaced Large Data" }),
+    actionSanitizer: (action) =>
+      action.type === "largeData/setLargeData"
+        ? { ...action, payload: "Replaced Large Data" }
+        : action,
+  },
+});
+
+const { setLargeData } = largeData.actions;
+
+store.dispatch(setLargeData(new ArrayBuffer(1e7)));
+store.dispatch(setLargeData(null));
+
+export default store;
